4d1e6af40cbc9092acdc373f75aef5e13ddb01ce
bk408 <bhavyakhatri1912@gmail.com>
2023-01-02 21:52:30 +0530

    Home Component created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './components/Home';
 import NavBar from './components/NavBar';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
 <NavBar />
+<Home />
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
   return (
 
-    <nav className="navbar navbar-expand-lg bg-body-tertiary py-3">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary py-3 bg-white shadow-sm">
       <div className="container">
         <a className="navbar-brand fw-bold fs-4" href="/">Ekart</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
