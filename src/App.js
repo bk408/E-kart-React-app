d8da5739cc893be21842d0179a5671b0d8cf1dcf
bk408 <bhavyakhatri1912@gmail.com>
2023-01-30 19:30:48 +0530

    Update the about component & Contact component created

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Products from './components/Products';
 import Product from './components/Product';
 import Cart from  './components/Cart';
 import About from './components/About';
+import Contact from './components/Contact';
 
 
 
@@ -29,7 +30,7 @@ function App() {
  <Route exact path="/products/:id" key="products/:id" element={<Product />} />
  <Route exact path="/cart" key="cart" element={<Cart />} />
  <Route exact path="/about" key="about" element={<About />} />
- 
+ <Route exact path="/contact" key="contact" element={<Contact />} />
  </Routes>
     </>
      
